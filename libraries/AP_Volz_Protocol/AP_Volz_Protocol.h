dbe30ddc407a0755fd798f5238047753c7d222fc
Iampete1 <iampete@hotmail.co.uk>
2022-01-02 18:20:21 +0000

    AP_Voltz_Protocol: move AP_SerialManager include to cpp

--- libraries/AP_Volz_Protocol/AP_Volz_Protocol.h
+++ libraries/AP_Volz_Protocol/AP_Volz_Protocol.h
@@ -34,10 +34,7 @@
 #pragma once
 
 #include <AP_HAL/AP_HAL.h>
-#include <AP_SerialManager/AP_SerialManager.h>
 #include <AP_Param/AP_Param.h>
-
-//#include <GCS_MAVLink/GCS.h>
 
 #define VOLZ_SCALE_VALUE 					(uint16_t)(VOLZ_EXTENDED_POSITION_MAX - VOLZ_EXTENDED_POSITION_MIN)	// Extended Position Data Format defines 100 as 0x0F80, which results in 1920 steps for +100 deg and 1920 steps for -100 degs meaning if you take movement a scaled between -1 ... 1 and multiply by 1920 you get the travel from center
 #define VOLZ_SET_EXTENDED_POSITION_CMD 		0xDC
